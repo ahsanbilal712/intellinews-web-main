944208d321d7c77606abc5952f0c9451bae387a0
Ahsan Bilal <ahsan.bilal.712@gmail.com>
2024-07-01 17:32:34 +0500

    Loading pages dynamically

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,129 +1,43 @@
-// import { getAllPosts } from "../../lib/api";
-// import HeadMeta from "../components/elements/HeadMeta";
-// import FooterOne from "../components/footer/FooterOne";
-// import HeaderOne from "../components/header/HeaderOne";
-// import PostSectionFive from "../components/post/PostSectionFive";
-// import PostSectionFour from "../components/post/PostSectionFour";
-// import PostSectionOne from "../components/post/PostSectionOne";
-// import PostSectionSix from "../components/post/PostSectionSix";
-// import PostSectionThree from "../components/post/PostSectionThree";
-// import PostSectionTwo from "../components/post/PostSectionTwo";
-//
-// const HomeOne = ({allPosts}) => {
-//
-//   return (
-//     <>
-//     <HeadMeta metaTitle="Home"/>
-//     <HeaderOne />
-//     <PostSectionOne postData={allPosts} />
-//     {/*<PostSectionTwo postData={allPosts} />*/}
-//     {/*<PostSectionThree postData={allPosts} />*/}
-//     {/*<PostSectionFour postData={allPosts} />*/}
-//     {/*<PostSectionFive postData={allPosts} adBanner={true} />*/}
-//     {/*<PostSectionSix postData={allPosts}/>*/}
-//     <FooterOne />
-//     </>
-//    );
-// }
-//
-// export default HomeOne;
-//
-//
-// export async function getStaticProps() {
-//   const allPosts = getAllPosts([
-//     'postFormat',
-//     'trending',
-//     'story',
-//     'slug',
-//     'title',
-//     'excerpt',
-//     'featureImg',
-//     'cate',
-//     'cate_bg',
-//     'cate_img',
-//     'author_name',
-//     'date',
-//     'post_views',
-//     'post_share',
-//   ])
-//
-//   return {
-//     props: { allPosts }
-//   }
-// }
-//
 
 
-import { getAllPosts } from "../../lib/api";
+import Link from 'next/link';
 import HeadMeta from "../components/elements/HeadMeta";
 import FooterOne from "../components/footer/FooterOne";
 import HeaderTwo from "../components/header/HeaderTwo";
-import PostSectionFive from "../components/post/PostSectionFive";
-import PostSectionSeven from "../components/post/PostSectionSeven";
-import PostSectionSix from "../components/post/PostSectionSix";
-import PostSectionThree from "../components/post/PostSectionThree";
+import useSWR from 'swr';
 
-//import SliderOne from "../components/slider/SliderOne";
+const fetcher = url => fetch(url).then(res => res.json());
 
-const HomeTwo = ({allPosts}) => {
+function HomeTwo() {
+  const { data, error } = useSWR('/api/news', fetcher);
+
+  if (error) return <div>Failed to load data.</div>;
+  if (!data) return <div>Loading...</div>;
 
   return (
-      <>
-        <HeadMeta metaTitle="Home"/>
-        <HeaderTwo />
-        <PostSectionThree postData={allPosts} />
-        {/*<PostSectionSeven postData={allPosts} />*/}
-        <PostSectionFive postData={allPosts} />
-        {/*<PostSectionSix postData={allPosts} />*/}
-        <FooterOne />
-      </>
+    <>
+      <HeadMeta metaTitle="Home"/>
+      <HeaderTwo />
+      <div>
+        <ul>
+          {data.map(news => (
+            <li key={news._id}>
+              <Link href={`/news/${news._id}`}>
+                <a><h2>{news.Headline}</h2></a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <FooterOne />
+    </>
   );
 }
 
 export default HomeTwo;
 
-export async function getStaticProps() {
-  const allPosts = await getAllPosts([
-    '_id',
-    'Headline',
-    'Summary' // Specify more fields as necessary
-  ]);
 
-  // Serialize the data
-  const posts = allPosts.map(post => ({
-    id: post._id.toString(), // Convert ObjectId to string
-    headline: post.Headline,
-    summary: post.Summary
-  }));
 
-  return {
-    props: { allPosts: posts }
-  };
-}
 
-// import useSWR from 'swr';
 
-// const fetcher = url => fetch(url).then(res => res.json());
 
-// function HomePage() {
-//   const { data, error } = useSWR('/api/news', fetcher);
-
-//   if (error) return <div>Failed to load data.</div>;
-//   if (!data) return <div>Loading...</div>;
-
-//   return (
-//     <div>
-//       <h1>Latest News Summaries</h1>
-//       <ul>
-//         {data.map(news => (
-//           <li key={news._id}>
-//             <h2>{news.Headline}</h2>
-//             <p>{news.Summary}</p>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default HomePage;
--- src/pages/news/[id].js
+++ src/pages/news/[id].js
@@ -1,33 +1,41 @@
-import { useRouter } from 'next/router';
-import { getPostById } from '../../../lib/api';
+import { MongoClient, ObjectId } from 'mongodb';
+import HeadMeta from '../../components/elements/HeadMeta';
+import FooterOne from '../../components/footer/FooterOne';
+import HeaderTwo from '../../components/header/HeaderTwo';
 
-const NewsArticle = ({ news }) => {
-  const router = useRouter();
-  if (router.isFallback) {
-    return <div>Loading...</div>;
+const NewsPage = ({ news }) => {
+  if (!news) {
+    return <div>No news found.</div>;
   }
 
   return (
     <>
-      <h1>{news.Headline}</h1>
-      <p>{news.Summary}</p>
+      <HeadMeta metaTitle={news.Headline}/>
+      <HeaderTwo />
+      <div>
+        <h1>{news.Headline}</h1>
+        <p>{news.Summary}</p>
+      </div>
+      <FooterOne />
     </>
   );
+};
+
+export async function getServerSideProps(context) {
+  const { id } = context.params;
+  const client = await MongoClient.connect(process.env.MONGODB_URI);
+  const db = client.db('db');
+
+  // Use the 'new' keyword with ObjectId
+  const news = await db.collection('news_summaries').findOne({_id: new ObjectId(id)});
+
+  client.close();
+
+  return {
+    props: {
+      news: news ? JSON.parse(JSON.stringify(news)) : null
+    }
+  };
 }
 
-export default NewsArticle;
-
-export async function getStaticPaths() {
-  const ids = await getPostIds();
-  const paths = ids.map(id => ({
-    params: { id },
-  }));
-
-  return { paths, fallback: true };
-}
-
-export async function getStaticProps({ params }) {
-  const news = await getPostById(params.id);
-
-  return { props: { news } };
-}
+export default NewsPage;
