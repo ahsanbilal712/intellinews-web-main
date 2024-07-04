adf29c711ce8b5f58ba5cb4cc0d5686f4930ab1d
Ahsan Bilal <ahsan.bilal.712@gmail.com>
2024-07-04 13:58:11 +0500

    updated categories nav bar

--- src/components/header/HeaderTwo.jsx
+++ src/components/header/HeaderTwo.jsx
@@ -1,94 +1,92 @@
+
 import React, { useState, useRef, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import SocialLink from "../../data/social/SocialLink.json";
-import MenuData from "../../data/menu/HeaderMenu.json";
 import OffcanvasMenu from "./OffcanvasMenu";
 
 const HeaderTwo = () => {
- 
-// Main Menu Toggle 
-var menuRef = useRef();
+  const [categories, setCategories] = useState(["Pakistan", "Politics", "International","Sports", "Entertainment","Weather", "Science"]); // Example categories
+  const menuRef = useRef();
 
-const toggleDropdownMenu = () => {
-  const dropdownSelect = menuRef.current.childNodes;
-  let dropdownList = [];
+  // Existing Menu Toggle Functionality
+  const toggleDropdownMenu = () => {
+    const dropdownSelect = menuRef.current.childNodes;
+    let dropdownList = [];
   
-  for (let i = 0; i < dropdownSelect.length; i++) {
-    const element = dropdownSelect[i];
-    if (element.classList.contains("has-dropdown")) {
-      dropdownList.push(element)
+    for (let i = 0; i < dropdownSelect.length; i++) {
+      const element = dropdownSelect[i];
+      if (element.classList.contains("has-dropdown")) {
+        dropdownList.push(element);
+      }
     }
+
+    dropdownList.forEach(element => {
+      element.children[0].addEventListener('click', () => {
+        if (element.classList.contains('active')) {
+          element.classList.remove('active');
+          element.childNodes[1].classList.remove('opened');
+        } else {
+          dropdownList.forEach(submenu => {
+            if (element !== submenu) {
+              submenu.classList.remove('active');
+              submenu.childNodes[1].classList.remove('opened');
+            } else {
+              submenu.classList.add('active');
+              submenu.childNodes[1].classList.add('opened');
+            }
+          });
+        }
+      });
+    });
   }
 
-  dropdownList.forEach(element => {
+  useEffect(() => {
+    toggleDropdownMenu();
+  }, []);
 
-    element.children[0].addEventListener('click', () => {
+ // Offcanvas Menu
 
-      if (element.classList.contains('active')) {
-        element.classList.remove('active');
-        element.childNodes[1].classList.remove('opened');
-      }else {
-        dropdownList.forEach(submenu => {
-          if (element !== submenu) {
-            submenu.classList.remove('active');
-            submenu.childNodes[1].classList.remove('opened');
-          }else {
-            submenu.classList.add('active');
-            submenu.childNodes[1].classList.add('opened');
-          }
-        })
+ const [show, setShow] = useState(false);
+ const handleClose = () => setShow(false);
+ const handleShow = () => setShow(true);
+
+ // Header Search
+ const [searchshow, setSearchShow] = useState(false);
+
+ const headerSearchShow = () => {
+   setSearchShow(true);
+ };
+ const headerSearchClose = () => {
+   setSearchShow(false);
+ };
+
+// Mobile Menu Toggle 
+const [mobileToggle, setMobileToggle] = useState(false);
+
+const MobileMenuToggler = () => {
+  setMobileToggle(!mobileToggle);
+  const HtmlTag = document.querySelector('html');
+  const menuSelect = document.querySelectorAll('.main-navigation li');
+
+  if (HtmlTag.classList.contains('main-menu-opened')) {
+    HtmlTag.classList.remove('main-menu-opened');
+  }else {
+    setTimeout(() => {
+      HtmlTag.classList.add('main-menu-opened');
+    }, 800)
+  }
+
+  menuSelect.forEach(element => {
+    element.addEventListener('click', function () {
+      if (!element.classList.contains('has-dropdown')) {
+        HtmlTag.classList.remove('main-menu-opened');
+        setMobileToggle(false);
       }
     })
+
   });
 }
-
-useEffect(() => {
-  toggleDropdownMenu();
-}, []);
-
-  // Offcanvas Menu
-
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  // Header Search
-  const [searchshow, setSearchShow] = useState(false);
-
-  const headerSearchShow = () => {
-    setSearchShow(true);
-  };
-  const headerSearchClose = () => {
-    setSearchShow(false);
-  };
-
- // Mobile Menu Toggle 
- const [mobileToggle, setMobileToggle] = useState(false);
-
- const MobileMenuToggler = () => {
-   setMobileToggle(!mobileToggle);
-   const HtmlTag = document.querySelector('html');
-   const menuSelect = document.querySelectorAll('.main-navigation li');
-
-   if (HtmlTag.classList.contains('main-menu-opened')) {
-     HtmlTag.classList.remove('main-menu-opened');
-   }else {
-     setTimeout(() => {
-       HtmlTag.classList.add('main-menu-opened');
-     }, 800)
-   }
-
-   menuSelect.forEach(element => {
-     element.addEventListener('click', function () {
-       if (!element.classList.contains('has-dropdown')) {
-         HtmlTag.classList.remove('main-menu-opened');
-         setMobileToggle(false);
-       }
-     })
-
-   });
- }
 
   return (
     <>
@@ -96,7 +94,7 @@ useEffect(() => {
       <header className="page-header">
         <div className="header-top header-top__style-two bg-grey-dark-seven">
           <div className="container">
-            <div className="row justify-content-between align-items-center">
+          <div className="row justify-content-between align-items-center">
               <div className="col-md-4">
                 <ul className="header-top-nav list-inline justify-content-center justify-content-md-start">
                   <li>
@@ -159,90 +157,25 @@ useEffect(() => {
         <nav className="navbar bg-grey-dark-one navbar__style-four">
           <div className="container">
             <div className="navbar-inner justify-content-between">
-				<div className="navbar-toggler-wrapper">
-					<button className="side-nav-toggler" onClick={handleShow}>
-						<span />
-						<span />
-						<span />
-					</button>
-				</div>
-
+              <div className="navbar-toggler-wrapper">
+                <button className="side-nav-toggler" onClick={handleShow}>
+                  <span />
+                  <span />
+                  <span />
+                </button>
+              </div>
               <div className="main-nav-wrapper">
                 <ul className="main-navigation list-inline" ref={menuRef}>
-                  {MenuData.map((data, index) =>
-                    data.submenu ? (
-                      <li className="has-dropdown" key={index}>
-                        <Link href={data.path}>
-                          <a>{data.label}</a>
-                        </Link>
-                        <ul className="submenu">
-                          {data.submenu.map((data, index) =>
-                            data.thirdmenu ? (
-                              <li className="has-dropdown" key={index}>
-                                <Link href={data.subpath}>
-                                  <a>{data.sublabel}</a>
-                                </Link>
-                                <ul className="submenu">
-                                  {data.thirdmenu.map((data, index) => (
-                                    <li key={index}>
-                                      <Link href={data.tpath}>
-                                        <a>{data.tlabel}</a>
-                                      </Link>
-                                    </li>
-                                  ))}
-                                </ul>
-                              </li>
-                            ) : (
-                              <li key={index}>
-                                <Link href={data.subpath}>
-                                  <a>{data.sublabel}</a>
-                                </Link>
-                              </li>
-                            )
-                          )}
-                        </ul>
-                      </li>
-                    ) : (
-                      <li key={index}>
-                        <Link href={data.path}>
-                          <a>{data.label}</a>
-                        </Link>
-                      </li>
-                    )
-                  )}
+                 
+                  {/* Dynamically add category links */}
+                  {categories.map((category, index) => (
+                    <li key={`category-${index}`}>
+                      <Link href={`/categories/${category}`}>
+                        <a>{category}</a>
+                      </Link>
+                    </li>
+                  ))}
                 </ul>
-              </div>
-              <div className="navbar-extra-features">
-                <form
-                  action="#"
-                  className={`navbar-search ${
-                    searchshow ? "show-nav-search" : ""
-                  }`}
-                >
-                  <div className="search-field">
-                    <input
-                      type="text"
-                      className="navbar-search-field"
-                      placeholder="Search Here..."
-                    />
-                    <button className="navbar-search-btn" type="button">
-                      <i className="fal fa-search" />
-                    </button>
-                  </div>
-                  <span
-                    className="navbar-search-close"
-                    onClick={headerSearchClose}
-                  >
-                    <i className="fal fa-times" />
-                  </span>
-                </form>
-
-                <button
-                  className="nav-search-field-toggler mr-0"
-                  onClick={headerSearchShow}
-                >
-                  <i className="far fa-search" />
-                </button>
               </div>
               <div
                 className={`main-nav-toggler d-block d-lg-none ${mobileToggle ? "expanded" : ""}`}>
