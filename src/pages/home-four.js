48bd23c0d50cac742dd9b2268a14de2def267724
Ahsan Bilal <ahsan.bilal.712@gmail.com>
2024-08-03 17:42:37 +0500

    Commented slugs for deployement

--- src/pages/home-four.js
+++ src/pages/home-four.js
@@ -1,46 +1,44 @@
-import { getAllPosts } from "../../lib/api";
-import HeadMeta from "../components/elements/HeadMeta";
-import FooterTwo from "../components/footer/FooterTwo";
-import HeaderFour from "../components/header/HeaderFour";
-import PostSectionSix from "../components/post/PostSectionEight";
-import SliderThree from "../components/slider/SliderThree";
+// import { getAllPosts } from "../../lib/api";
+// import HeadMeta from "../components/elements/HeadMeta";
+// import FooterTwo from "../components/footer/FooterTwo";
+// import HeaderFour from "../components/header/HeaderFour";
+// import PostSectionSix from "../components/post/PostSectionEight";
+// import SliderThree from "../components/slider/SliderThree";
 
-const HomeFour = ({allPosts}) => {
+// const HomeFour = ({allPosts}) => {
 
-  return ( 
-    <>
-    <HeadMeta metaTitle="Home Four"/>
-    <HeaderFour />
-    <SliderThree postData={allPosts} />
-    <PostSectionSix postData={allPosts} />
-    <FooterTwo />
-    </>
-   );
-}
- 
-export default HomeFour;
+//   return (
+//     <>
+//     <HeadMeta metaTitle="Home Four"/>
+//     <HeaderFour />
+//     <SliderThree postData={allPosts} />
+//     <PostSectionSix postData={allPosts} />
+//     <FooterTwo />
+//     </>
+//    );
+// }
 
+// export default HomeFour;
 
-export async function getStaticProps() {
-  const allPosts = getAllPosts([
-    'slug',
-    'postFormat',
-    'story',
-    'trending',
-    'title',
-    'excerpt',
-    'featureImg',
-    'cate',
-    'cate_bg',
-    'cate_img',
-    'author_name',
-    'date',
-    'post_views',
-    'post_share',
-  ])
-  
-  return {
-    props: { allPosts }
-  }
-}
+// export async function getStaticProps() {
+//   const allPosts = getAllPosts([
+//     'slug',
+//     'postFormat',
+//     'story',
+//     'trending',
+//     'title',
+//     'excerpt',
+//     'featureImg',
+//     'cate',
+//     'cate_bg',
+//     'cate_img',
+//     'author_name',
+//     'date',
+//     'post_views',
+//     'post_share',
+//   ])
 
+//   return {
+//     props: { allPosts }
+//   }
+// }
